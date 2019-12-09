cdaedc68317b5fd294473139e3c4cd97ed0586f3
Baytik Tumanbaev <jumka.wows@gmail.com>
2019-12-09 15:51:16 +0600

    Сделал Пост запрос

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -6,7 +6,29 @@ class Blog extends Component {
 
     state = {
         posts: [],
+        author: '',
+        message: ''
     };
+
+     postMessages = () => {
+        const url = 'http://146.185.154.90:8000/messages';
+        const data = new URLSearchParams();
+        data.set('message', this.state.message);
+        data.set('author', this.state.author);
+        fetch (url, {
+            method: 'post',
+            body: data,
+        })
+    };
+
+     interval = () => {
+       setInterval(() => {
+
+       }, 1000)
+     };
+
+    changeHandler = (e) => this.setState({message: e.target.value});
+    inputName = (event) => this.setState({author: event.target.value});
 
     async componentDidMount() {
         const response = await fetch('http://146.185.154.90:8000/messages');
@@ -23,9 +45,9 @@ class Blog extends Component {
                 {this.state.posts.map(post => (
                     <Post message={post.message} author={post.author}/>
                 ))}
-                <input type="text" className="main-input" placeholder="Ваш Текст"/>
-                <input type="text" className="user-input" placeholder="Ваше Имя"/>
-                <button className="btn-1">Отправить</button>
+                <input type="text" className="main-input" placeholder="Ваш Текст" onChange={this.changeHandler}/>
+                <input type="text" className="user-input" placeholder="Ваше Имя" onChange={this.inputName}/>
+                <button className="btn-1" onClick={this.postMessages}>Отправить</button>
             </div>
         );
     }
