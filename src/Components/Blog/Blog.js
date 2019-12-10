46f24efc8604d1cbc29635f3b8c94ae64e9b0a13
Baytik Tumanbaev <jumka.wows@gmail.com>
2019-12-10 18:29:34 +0600

    Закончил домашку

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,53 +1,73 @@
 import React, {Component} from 'react';
 import Post from "../Post/Post";
 import './Blog.css';
-
 class Blog extends Component {
-
+    lastDateTime = null;
+    interval = null;
+    url = "http://146.185.154.90:8000/messages";
     state = {
         posts: [],
         author: '',
-        message: ''
+        message: '',
     };
-
-     postMessages = () => {
-        const url = 'http://146.185.154.90:8000/messages';
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.getMessages()
+        }, 2000)
+    }
+    getPost = (e) => {
+        e.preventDefault();
         const data = new URLSearchParams();
         data.set('message', this.state.message);
         data.set('author', this.state.author);
-        fetch (url, {
+        fetch (this.url, {
             method: 'post',
             body: data,
         })
     };
+    author = event => this.setState({author: event.target.value});
+    message = event => this.setState({message: event.target.value});
+    renderOnPage = ()=> {
+        return this.state.posts.map((post, index) => (
+            <Post key={index} message={post.message} author={post.author}/>
+        ))
+    };
 
-     interval = () => {
-       setInterval(() => {
-
-       }, 1000)
-     };
-
-    changeHandler = (e) => this.setState({message: e.target.value});
-    inputName = (event) => this.setState({author: event.target.value});
-
-    async componentDidMount() {
-        const response = await fetch('http://146.185.154.90:8000/messages');
-        if (response.ok) {
-            const posts = await response.json();
-            this.setState({posts});
+    getMessages = async (dateTime) => {
+        if (dateTime === undefined) {
+            this.url = 'http://146.185.154.90:8000/messages';
+        } else {
+            this.url = 'http://146.185.154.90:8000/messages?datetime=' + this.lastDateTime
         }
+        const response = await fetch(this.url);
+        if (response.ok) {
+            const result = await response.json();
+            if(result.length !== 0) {
+                let posts = [...this.state.posts];
+                this.lastDateTime = result[result.length - 1].datetime;
+                this.setState({posts: posts.concat(result).reverse()});
+            }
+        }
+    };
+
+    componentDidUpdate() {
+        clearInterval(this.interval);
+        this.interval = setInterval(() =>  {
+            this.getMessages(this.lastDateTime)
+        },2000)
     }
 
+
     render() {
-        console.log(this.state.posts);
         return (
             <div className="Blog">
-                {this.state.posts.map(post => (
-                    <Post message={post.message} author={post.author}/>
-                ))}
-                <input type="text" className="main-input" placeholder="Ваш Текст" onChange={this.changeHandler}/>
-                <input type="text" className="user-input" placeholder="Ваше Имя" onChange={this.inputName}/>
-                <button className="btn-1" onClick={this.postMessages}>Отправить</button>
+                <form action="" onSubmit={this.getPost}>
+                    <input type="text" className="main-input" placeholder="Ваш Текст" onChange={this.message}/>
+                    <input type="text" className="user-input" placeholder="Ваше Имя" onChange={this.author}/>
+                    <button className="btn-1" type='submit'>Отправить</button>
+                </form>
+                {this.renderOnPage()}
+
             </div>
         );
     }
